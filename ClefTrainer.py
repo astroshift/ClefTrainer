df12502eb9bc4fe3b6e56b1d8cb46521caf8d1c6
astroshift <dank.work@tuta.io>
2022-09-05 22:09:42 -0600

    Refactored and implemented treble clef

--- ClefTrainer.py
+++ ClefTrainer.py
@@ -1,7 +1,3 @@
-# ------------------------------------------------
-# Bass Clef (Treble soon) Training Application
-# Take images from folder and then quiz user
-
 import os
 import sys
 import random
@@ -16,8 +12,10 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QHBoxLayout,
     QStackedWidget,
+    QComboBox,
 )
 
+# base path
 bundle_dir = os.path.dirname(__file__)
 
 try:
@@ -36,47 +34,97 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(
             QIcon(os.path.join(bundle_dir, 'icons', 'ct-icon.png')))
 
-        pageLayout = QVBoxLayout(self)
-        buttonLayout = QHBoxLayout(self)
+        self.notes = [QPushButton('A'), QPushButton('B'), QPushButton('C'),
+                      QPushButton('D'), QPushButton('E'), QPushButton('F'),
+                      QPushButton('G')
+                      ]
+
+        # used to save clef images
         self.imageStack = QStackedWidget(self)
+        self.chosen_clef = []
 
-        notes = [QPushButton('A'), QPushButton('B'), QPushButton('C'),
-                 QPushButton('D'), QPushButton('E'), QPushButton('F'),
-                 QPushButton('G')
-                 ]
+        # guessed note is made globally available
+        self.note_to_guess = None
 
-        b = 'bass-clef'
-        t = 'treble-clef'
+        # select which clef (default none), begin by prompting user
+        self.user_choice = None
+        self.spawn_prompt()
 
-        bass_clef = os.listdir(os.path.join(bundle_dir, b))
-        treble_clef = os.listdir(os.path.join(bundle_dir, t))
+    def spawn_prompt(self):
+        init_prompt = QWidget()
+        prompt_layout = QVBoxLayout()
+        button_layout = QHBoxLayout()
 
-        # select which clef
-        self.note_list = bass_clef
+        '''
+        ## primitive rounds implementation, come back later
+        rounds_layout = QHBoxLayout()
+        
+    
+        self.selected_rounds = 0
 
-        pageLayout.addWidget(self.imageStack)
-        pageLayout.addLayout(buttonLayout)
+        dropdown_menu = QComboBox()
+        for i in range(0, 100):
+            dropdown_menu.addItem(str(i))
+            dropdown_menu.activated(i).connect(self.dropdown_select())
+
+        rounds_layout.addWidget(QLabel("Rounds Played: "))
+        rounds_layout.addWidget(dropdown_menu)
+        '''
+
+        buttons = [QPushButton('Treble'), QPushButton('Bass')]
+        buttons[0].clicked.connect(self.select_treble)
+        buttons[1].clicked.connect(self.select_bass)
+        for x in buttons:
+            button_layout.addWidget(x)
+
+        prompt_layout.addLayout(button_layout)
+        init_prompt.setLayout(prompt_layout)
+
+        self.setCentralWidget(init_prompt)
+        self.show()
+
+    def select_treble(self):
+        self.spawn_clef('treble-clef')
+
+    def select_bass(self):
+        self.spawn_clef('bass-clef')
+
+    def generate_note(self):
+        # randomize note with number for repeat usage
+        note_num = random.randint(0, 16)
+        self.note_to_guess = self.chosen_clef[note_num][0]
+        self.imageStack.setCurrentIndex(note_num)
+
+    def check_guess(self):
+        # compare pressed note with guessed note
+        sending_button = self.sender()
+        if sending_button.text() == self.note_to_guess:
+            self.generate_note()
+
+    def spawn_clef(self, user_choice):
+        pageLayout = QVBoxLayout()
+        buttonLayout = QHBoxLayout()
+
+        self.chosen_clef = os.listdir(os.path.join(bundle_dir, user_choice))
 
         # iterate image list and add to stack
-        for loc in self.note_list:
-            if self.note_list == bass_clef:
-                pixmap = QPixmap(os.path.join(bundle_dir, b, loc))
-            else:
-                pixmap = QPixmap(os.path.join(bundle_dir, t, loc))
+        for loc in self.chosen_clef:
+            pixmap = QPixmap(os.path.join(bundle_dir, user_choice, loc))
             image = QLabel(self)
             image.setPixmap(pixmap)
             image.setScaledContents(True)
             self.imageStack.addWidget(image)
 
-        # add all buttons and enable functionality
-        for note in notes:
-            buttonLayout.addWidget(note)
-            note.clicked.connect(self.button_pressed)
-
-        # generate a note name to guess and random number for further usage
-        self.note_to_guess = None
-        self.note_num = None
+        # preemptively generate new note for guessing
         self.generate_note()
+
+        # add all buttons and enable functionality
+        for note in self.notes:
+            buttonLayout.addWidget(note)
+            note.clicked.connect(self.check_guess)
+
+        pageLayout.addWidget(self.imageStack)
+        pageLayout.addLayout(buttonLayout)
 
         container = QWidget()
         container.setLayout(pageLayout)
@@ -84,15 +132,6 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
         self.show()
 
-    def button_pressed(self):
-        sending_button = self.sender()
-        if sending_button.text() == self.note_to_guess:
-            self.generate_note()
-
-    def generate_note(self):
-        self.note_num = random.randint(0, 16)
-        self.note_to_guess = self.note_list[self.note_num][0]
-        self.imageStack.setCurrentIndex(self.note_num)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
